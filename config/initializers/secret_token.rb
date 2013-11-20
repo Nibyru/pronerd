8a9dbe5462ce51710720443cc96fbe645f13c0ce
Crank <mario@nibyru.com>
2013-11-20 18:25:19 -0300

    Comienzo de Aplicacion

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pronerd::Application.config.secret_key_base = 'f756dd5bf44af5f19787158a81caea091fffd5fadf3cb62d0f0dfb5b836aeb5ee09fdce629274b0f74994ee97ab2ce298c6c1fd887a98dd4455291778faacd55'
